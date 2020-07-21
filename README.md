ec813e877ce78b2b23bfdc04f7b0f82e1eeab34c
root <root@e2e-64-65.e2enetworks.net.in>
2020-07-21 13:50:02 +0530

    Version 1

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# Handwriting-Recognition
+# Handwritten Text Recognition using Tensorflow
 
-Target: To achieve 90% accuracy on handwriting-recongition.
+The work here is an implementation of Stage 3 Dual stream architecture in [Fully Convolutional Networks for Handwriting Recognition](https://arxiv.org/abs/1907.04888). 
+
+The neural network takes in handwriting sample of unknown length and outputs an arbitary sequence of characters. We achieved an accuracy of 34% and further improvements have to be made.
+
+## Dataset
+
+The dataset used here is the IAM dataset available at this [website](http://www.fki.inf.unibe.ch/databases/iam-handwriting-database). Then follow these instructions:
+1. Create a directory `Data` inside this directory.
+2. Download `ascii/words.txt`  and place the files `words.txt` in `Data` directory.
+3. Download `words/words.tgz` and place the contents of the extracted file in 'Data/words' directory.
+
+## Model Overview:
+The dual stream architecture consists of three neural networks. Two streams are convolutional neural network rchitectures which are used to capture the local context and global context.
+The output of the two streams are combined and passed to the third stream which classifies the outputs of neural networks into symbols. We have used infinity-norm as loss function. Cross-entropy loss can also be used as loss function and it is to be tested.
+
+
+## Other References:
+
+\[1\] [SimpleHTR](https://github.com/githubharald/SimpleHTR)
+
+Disclaimer: We are in no way related to the authors of the paper.
--- main.py
+++ main.py
@@ -1,43 +1,59 @@
-from collections import defaultdict, Counter
-import random, cv2
-import numpy as np
-import tensorflow as tf
+#!/usr/bin/env python3
 
-import DataLoader, models
-#The model library define all model requirements like nerual network architecture, optimizer function, loss function.
+import os, random, cv2
+import numpy as np
+from collections import defaultdict, Counter
+from datetime import datetime
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+from DataLoader import DataLoader
+from Models import Models
 
 def train(ts_model, ms_model, bs_model, train_data, optimizer):
+    f = open('train.LOG', 'a')
+    f.write('In training\n')
+    f.close()
+
+    save_path = '/root/arun/Handwriting-Recognition/models'
+
     _, labels_pos_map, pos_labels_map = find_unique_characters()
 
-    epochs = 100
-
+    epochs = 10
     train_imgs_ts, train_imgs_bs, train_labels, train_n  =\
          train_data['imgs_ts'], train_data['imgs_bs'], train_data['labels'], train_data['n']
 
-    for epoch in range(epochs):
+    for epoch in range(1, epochs+1):
+        f = open('train.LOG', 'a')
+        f.write('Epoch is {}\n'.format(epoch))
+        f.close()
+
         epoch_loss = 0
         cer = 0
+        indexes = np.arange(0, len(train_imgs_ts), 1)
+        np.random.shuffle(indexes)
 
-        for idx in range(len(train_imgs_ts)):
+        for i, idx in enumerate(indexes):
+            if (i%1000 == 0):
+                f = open('train.LOG', 'a')
+                f.write('Epoch {} i {}   '.format(epoch, i))
+                f.close()
+
             img_ts = tf.convert_to_tensor(train_imgs_ts[idx])  #top stream input
             img_bs = tf.convert_to_tensor(train_imgs_bs[idx])  # bottom stream input
             actual_word = train_labels[idx]
 
             with tf.GradientTape(persistent=True) as tape:
-
             # ms_model - middle stream model, ts - top stream model, bs_model - bottom stream model
                 out = ms_model(ts_model(img_ts) + bs_model(img_bs))
                 out = tf.reshape(out, (out.shape[2], out.shape[3]))
-
+                out = out / tf.norm(out, axis=1, keepdims=True)
                 predicted_word = list()
                 for i in range(1, len(out), 2):
                     predicted_word.append(pos_labels_map[np.argmax(out[i])])
                 predicted_word = ''.join(predicted_word)
 
-            # print (actual_word, predicted_word)
-            # loss = get_character_error_rate(actual_word, predicted_word)    ->  Not able to construct gradients using this method
-
-            #Note: Shape of target depends on output shape.
                 target = np.zeros(out.shape)
                 for i in range(1, out.shape[0], 2):
                     char = actual_word[(i - 1)//2]  # Shape of out is 2n
@@ -45,26 +61,36 @@ def train(ts_model, ms_model, bs_model, train_data, optimizer):
                     target[i][pos] = 1.0
                 target = tf.convert_to_tensor(target, dtype=tf.float32)
 
-                loss = models.norm_loss(target, out)
-           
+                loss = models.norm_inf_loss(target, out)
+
             grads_ms = tape.gradient(loss, ms_model.trainable_weights)
             grads_ts = tape.gradient(loss, ts_model.trainable_weights)
             grads_bs = tape.gradient(loss, bs_model.trainable_weights)
-            
+
+            grads_ms, _ = tf.clip_by_global_norm(grads_ms, 1.0)
+            grads_ts, _ = tf.clip_by_global_norm(grads_ts, 1.0)
+            grads_bs, _ = tf.clip_by_global_norm(grads_bs, 1.0)
+
+            grads_ms = adjust_gradient(grads_ms, epoch)
+            grads_ts = adjust_gradient(grads_ts, epoch)
+            grads_bs = adjust_gradient(grads_bs, epoch)
+
             optimizer.apply_gradients(zip(grads_ms, ms_model.trainable_weights))
             optimizer.apply_gradients(zip(grads_bs, bs_model.trainable_weights))
             optimizer.apply_gradients(zip(grads_ts, ts_model.trainable_weights))
+
             epoch_loss += loss
             cer += models.get_character_error_rate(actual_word, predicted_word)
 
-        epoch_loss = int(epoch_loss)
-        if (epoch % 10 == 0):
-            print ('Epoch {} Loss {}  CER {}'.format(epoch, epoch_loss, cer))
+    ts_model.save(os.path.join(save_path, 'ts_model.h5'))
+    ms_model.save(os.path.join(save_path, 'ms_model.h5'))
+    bs_model.save(os.path.join(save_path, 'bs_model.h5'))
+
 
     return (ts_model, ms_model, bs_model)
 
-
 def test(ts_model, ms_model, bs_model, test_data):
+    print ('In testing \n\n\n')
     _, labels_pos_map, pos_labels_map = find_unique_characters()
 
     test_imgs_ts, test_imgs_bs, test_labels, test_n =\
@@ -80,7 +106,7 @@ def test(ts_model, ms_model, bs_model, test_data):
 
         out = ms_model(ts_model(img_ts) + bs_model(img_bs))
         out = tf.reshape(out, (out.shape[2], out.shape[3]))
-
+        out = out / tf.norm(out, axis=1, keepdims=True)
         predicted_word = list()
         for i in range(1, len(out), 2):
             predicted_word.append(pos_labels_map[np.argmax(out[i])])
@@ -90,13 +116,17 @@ def test(ts_model, ms_model, bs_model, test_data):
         for i in range(len(actual_word)):
             if actual_word[i] == predicted_word[i]:
                 correct_prediction += 1
-    
+
+        if (idx < 5):
+            print (actual_word, predicted_word, sep=' ', end='   ')
+
+    print ('Total characters {} correct prediction {}'.format(total_characters, correct_prediction))
     print ("Character wise accuracy {}".format(correct_prediction*100 / total_characters))
     return (0)
 
 def find_unique_characters():
 #Finding Ns and creating a map of labels and index
-    _, labels = DataLoader.get_image_paths_labels()
+    _, labels = dataloader.get_image_paths_labels()
     unique_characters = list()
     for i, label in enumerate(labels):
         for char in label:
@@ -105,7 +135,7 @@ def find_unique_characters():
 
     unique_characters.sort()
     Ns = len(unique_characters)
-    
+
     pos_labels_map = {}
     labels_pos_map = {}
     for i, char in enumerate(unique_characters):
@@ -114,26 +144,57 @@ def find_unique_characters():
 
     return (Ns, labels_pos_map, pos_labels_map)
 
+def adjust_gradient(grads, epoch, eta=0.03, gamma=0.55):
+    #Adding gradient noise and clipping the gradients
+    var = eta / ((1 + epoch) ** gamma)
+    for i, grad in enumerate(grads):
+        grads[i] = grad + np.random.normal(0, var, grad.shape)
+    return (grads)
+
 def main():
+
     Ns, _, __ = find_unique_characters()
-    train_data, test_data = DataLoader.make_train_test()
+
+    start = datetime.now()
+    train_data, test_data = dataloader.make_train_test()
+    end = datetime.now()
+
+    f = open('main.LOG', 'a')
+    f.write('Time taken to load data is {} seconds'.format((end - start).seconds))
+    f.close()
 
     train_count = len(train_data['n'])
     test_count = len(test_data['n'])
     print ('Total samples in training data is {} and in test data is {}'.format(train_count, test_count))
-    
 
-    ts_model = models.get_top_stream()
-    bs_model = models.get_bottom_stream()
-    ms_model = models.get_middle_stream(Ns)
+    models_loc = '/root/arun/Handwriting-Recognition/models'
+
+    # files = os.listdir(models_loc)
+    # for file in files:
+    #     os.remove(os.path.join(models_loc, file))
+
+    if len(os.listdir(models_loc)) >= 3:
+        ts_model = tf.keras.models.load_model(os.path.join(models_loc, 'ts_model.h5'))
+        bs_model = tf.keras.models.load_model(os.path.join(models_loc, 'bs_model.h5'))
+        ms_model = tf.keras.models.load_model(os.path.join(models_loc, 'ms_model.h5'))
+    else:
+        ts_model = models.get_top_stream()
+        bs_model = models.get_bottom_stream()
+        ms_model = models.get_middle_stream(Ns)
 
     optimizer = models.get_optimizer()
-    print (type(optimizer))
 
+    start = datetime.now()
     ts_model, ms_model, bs_model = train(ts_model, ms_model, bs_model, train_data, optimizer)
+    end = datetime.now()
+    f = open('main.LOG', 'a')
+    f.write('Time trained {} seconds'.format((end - start).seconds))
+    f.close()
     test(ts_model, ms_model, bs_model, test_data)
 #Training model
 
 
 if __name__ == '__main__':
+    models = Models()
+    dataloader = DataLoader()
     main()
